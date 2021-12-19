1c8ef35c3b42c2f284e2054bb45126aae9d74d77
Abdulla Al Shahid <shahidabdulla.aas@gmail.com>
2021-12-19 04:48:23 -0800

    final-commit

--- technitian.java
+++ technitian.java
@@ -3,6 +3,7 @@ package uio;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
@@ -18,7 +19,9 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.sql.ResultSet;    
+import java.sql.ResultSet;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;    
 
 public class technetian {
 
@@ -34,6 +37,16 @@ public class technetian {
 	@SuppressWarnings("rawtypes")
 	private JComboBox c4;
 	private JTextField age;
+	private JTextField con;
+	private JCheckBox chk1;
+	private JCheckBox chk2;
+	private JCheckBox chk3;
+	private JCheckBox chk4;
+	public String x;
+	public String y;
+	public String z;
+	public String o;
+
 
 	/**
 	 * Launch the application.
@@ -64,10 +77,12 @@ public class technetian {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 876, 761);
+		frame.setBounds(100, 100, 876, 737);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
+		frame.setSize(876, 737);
+		frame.setLocationRelativeTo(null);  
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Administrator\\Downloads\\resizedImage (3).png"));
@@ -76,12 +91,12 @@ public class technetian {
 		
 		JLabel lblNewLabel_1 = new JLabel("Name");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_1.setBounds(240, 144, 126, 27);
+		lblNewLabel_1.setBounds(94, 201, 126, 27);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Date");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_2.setBounds(240, 224, 126, 27);
+		lblNewLabel_2.setBounds(449, 201, 126, 27);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		date = new JTextField();
@@ -93,23 +108,23 @@ public class technetian {
 			    date.setText(dtf.format(now));
 			}
 		});
-		date.setBounds(419, 224, 212, 27);
+		date.setBounds(571, 202, 212, 29);
 		frame.getContentPane().add(date);
 		date.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Doctor Name");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_3.setBounds(240, 262, 126, 27);
+		lblNewLabel_3.setBounds(449, 239, 126, 27);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("Major Disease");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_4.setBounds(240, 300, 126, 27);
+		lblNewLabel_4.setBounds(449, 277, 126, 27);
 		frame.getContentPane().add(lblNewLabel_4);
 			
 		JLabel lblNewLabel_5 = new JLabel("Blood Group");
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_5.setBounds(240, 338, 126, 27);
+		lblNewLabel_5.setBounds(294, 338, 126, 27);
 		frame.getContentPane().add(lblNewLabel_5);
 			
 		JSeparator separator = new JSeparator();
@@ -131,45 +146,94 @@ public class technetian {
 		frame.getContentPane().add(separator_3);
 		
 		medicine = new JTextField();
-		medicine.setBounds(198, 514, 499, 128);
+		medicine.setBounds(201, 515, 499, 100);
 		frame.getContentPane().add(medicine);
 		medicine.setColumns(10);
 		
-		JCheckBox chk1 = new JCheckBox("New check box");
+		chk1 = new JCheckBox("high blood pressure");
+		chk1.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == 1) {
+					x = "YES";
+				}else {
+					x = "NO";
+				}
+			}
+		});
+		
 		chk1.setBackground(Color.PINK);
-		chk1.setBounds(296, 411, 120, 23);
+		chk1.setBounds(240, 411, 166, 23);
 		frame.getContentPane().add(chk1);
 		
-		JCheckBox chk2 = new JCheckBox("New check box");
+		chk2 = new JCheckBox("being overweight");
+		chk2.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == 1) {
+					y = "YES";
+				}else {
+					y = "NO";
+				}
+			}
+		});
 		chk2.setBackground(Color.PINK);
-		chk2.setBounds(495, 411, 114, 23);
+		chk2.setBounds(495, 411, 155, 23);
 		frame.getContentPane().add(chk2);
 		
-		JCheckBox chk3 = new JCheckBox("New check box");
+		chk3 = new JCheckBox("diabetes");
+		chk3.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == 1) {
+					z = "YES";
+				}else {
+					z = "NO";
+				}
+			}
+		});
 		chk3.setBackground(Color.PINK);
-		chk3.setBounds(296, 458, 120, 23);
+		chk3.setBounds(240, 458, 166, 23);
 		frame.getContentPane().add(chk3);
 		
-		JCheckBox chk4 = new JCheckBox("New check box");
+		chk4 = new JCheckBox("HIV");
+		chk4.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == 1) {
+					o = "YES";
+				}else {
+					o = "NO";
+				}
+			}
+		});
 		chk4.setBackground(Color.PINK);
-		chk4.setBounds(495, 458, 114, 23);
+		chk4.setBounds(495, 458, 155, 23);
 		frame.getContentPane().add(chk4);
 		
 		JButton addrec = new JButton("ADD Record");
 		addrec.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				DbConnection conn = new DbConnection();
 				String name = (String)c1.getSelectedItem();
 				String setAge = age.getText();
 				String setDate = date.getText();
 				String doctorName = (String)c2.getSelectedItem();
 				String disease = (String)c3.getSelectedItem();
 				String bloodGroup = (String)c4.getSelectedItem();
-				
+				String pres = medicine.getText();
+				String cona = con.getText();
+				try {
+					String sql = "INSERT INTO `allpatientreport`(`Name`, `Age`, `Date`, `DoctorName`, `Disease`, `BloodGroup`, `hbp`, `ow`, `dia`, `hiv`, `prescription`,`contact`) VALUES ('"+name+"','"+setAge+"','"+setDate+"','"+doctorName+"','"+disease+"','"+bloodGroup+"','"+x+"','"+y+"','"+z+"','"+o+"','"+pres+"','"+cona+"')";
+					conn.s.executeUpdate(sql);
+					JOptionPane.showMessageDialog(null, "Record Added Successfully");
+					  new technetian();
+					  technetian.main(null);
+					  frame.dispose();
+				}catch(Exception error) {
+					JOptionPane.showMessageDialog(null, error);
+				}
 			}
 		});
 		addrec.setFont(new Font("Tahoma", Font.BOLD, 14));
 		addrec.setBackground(Color.PINK);
-		addrec.setBounds(197, 653, 500, 36);
+		addrec.setBounds(197, 626, 138, 36);
 		frame.getContentPane().add(addrec);
 		
 		c1 = new JComboBox();
@@ -188,7 +252,7 @@ public class technetian {
 				}
 			}
 		});
-		c1.setBounds(419, 146, 212, 27);
+		c1.setBounds(201, 203, 212, 27);
 		frame.getContentPane().add(c1);
 		
 		c2 = new JComboBox();
@@ -207,7 +271,7 @@ public class technetian {
 				}
 			}
 		});
-		c2.setBounds(419, 262, 212, 27);
+		c2.setBounds(571, 241, 212, 27);
 		frame.getContentPane().add(c2);
 		
 		c3 = new JComboBox();
@@ -226,7 +290,7 @@ public class technetian {
 				}
 			}
 		});
-		c3.setBounds(419, 300, 212, 27);
+		c3.setBounds(571, 279, 212, 27);
 		frame.getContentPane().add(c3);
 		
 		c4 = new JComboBox();
@@ -245,22 +309,78 @@ public class technetian {
 				}
 			}
 		});
-		c4.setBounds(419, 338, 212, 27);
+		c4.setBounds(414, 338, 212, 27);
 		frame.getContentPane().add(c4);
 		
 		JLabel lblNewLabel_7 = new JLabel("Age");
 		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_7.setBounds(240, 182, 114, 31);
+		lblNewLabel_7.setBounds(95, 237, 114, 31);
 		frame.getContentPane().add(lblNewLabel_7);
 		
 		age = new JTextField();
-		age.setBounds(419, 184, 212, 29);
+		age.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				DbConnection cont = new DbConnection();
+				String user = (String)c1.getSelectedItem();
+				String sql = "SELECT `age` FROM `patient` WHERE username='"+user+"'";
+				try {
+					ResultSet rs = cont.s.executeQuery(sql);
+					while(rs.next()) {
+						age.setText(rs.getString("age"));
+					}
+				}catch(Exception error) {
+					
+				}
+				
+			}
+		});
+		age.setBounds(201, 240, 212, 29);
 		frame.getContentPane().add(age);
 		age.setColumns(10);
 		
+		JLabel lblNewLabel_8 = new JLabel("Contact");
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_8.setBounds(94, 277, 79, 27);
+		frame.getContentPane().add(lblNewLabel_8);
+		
+		con = new JTextField();
+		con.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				DbConnection cont = new DbConnection();
+				String user = (String)c1.getSelectedItem();
+				String sql = "SELECT `contact` FROM `patient` WHERE username='"+user+"'";
+				try {
+					ResultSet rs = cont.s.executeQuery(sql);
+					while(rs.next()) {
+						con.setText(rs.getString("contact"));
+					}
+				}catch(Exception error) {
+					
+				}
+			}
+		});
+		con.setBounds(201, 282, 212, 29);
+		frame.getContentPane().add(con);
+		con.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Logout");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new fg();
+				uio.fg.main(null);
+				frame.dispose();
+			}
+		});
+		btnNewButton.setBackground(Color.PINK);
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnNewButton.setBounds(571, 626, 127, 36);
+		frame.getContentPane().add(btnNewButton);
+		
 		JLabel lblNewLabel_6 = new JLabel("");
 		lblNewLabel_6.setIcon(new ImageIcon("C:\\Users\\Administrator\\Desktop\\Gradient_builder_2.jpg"));
-		lblNewLabel_6.setBounds(0, 0, 860, 722);
+		lblNewLabel_6.setBounds(0, 0, 872, 698);
 		frame.getContentPane().add(lblNewLabel_6);
 	}
 }
